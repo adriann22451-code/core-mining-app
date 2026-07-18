7f7a4303b0e3e34b872fe3fcce4d61e7b9ce0e18
adriann22451-code <adriann22451@gmail.com>
2026-07-18 20:13:07 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import CoreMiningApp from "./CoreMiningApp.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CoreMiningApp />
+    <TonConnectUIProvider manifestUrl="https://core-mining-app.vercel.app/tonconnect-manifest.json">
+      <CoreMiningApp />
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
